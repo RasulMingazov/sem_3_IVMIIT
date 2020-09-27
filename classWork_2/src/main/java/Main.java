069c069861a0f89fa081f7ff3e5f9ba8f2d194a6
RasulMingazov <59110467+RasulMingazov@users.noreply.github.com>
2020-09-27 21:41:43 +0300

    -

--- classWork_2/src/main/java/Main.java
+++ classWork_2/src/main/java/Main.java
@@ -1,4 +1,7 @@
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.lang.reflect.Type;
 import java.util.Arrays;
 
 public class Main {
@@ -6,14 +9,73 @@ public class Main {
 
         Class jFreeClass = Class.forName("org.jfree.chart.JFreeChart");
 
-        Class<?>[] jFreeInterfaces = jFreeClass.getInterfaces();
-        System.out.println(Arrays.toString(jFreeInterfaces));
+        printSuperclass(jFreeClass); // имя родительского класса (суперкласса)
+        System.out.println("\n");
 
-        Field[] fields = jFreeClass.getFields();
+        printInterfaces(jFreeClass); //список реализуемых интерфейсов
+        System.out.println("\n");
 
+        printFieldAndType(jFreeClass); //список полей и их типы
+        System.out.println("\n");
+
+        printOnlyStaticFields(jFreeClass); //список только статических полей
+        System.out.println("\n");
+
+        printMethods(jFreeClass);
+        System.out.println("\n");
+
+
+    }
+    static void printSuperclass(Class c) {
+        System.out.println("SuperClass - " + c.getSuperclass().getName());
+    }
+
+    static void printInterfaces(Class c) {
+        Class<?>[] interfaces = c.getInterfaces();
+        int i = 0;
+        for (Class interfaceO: interfaces) {
+            System.out.println("Interface number " + i + " - " + interfaceO.getName());
+            i++;
+        }
+    }
+
+    static void printFieldAndType(Class c) {
+        Field[] fields = c.getDeclaredFields();
+        int i = 0;
         for (Field field: fields) {
-            System.out.println("Поле: " + field.getName());
-            System.out.println("Тип: " + field.getType().getName());
+            System.out.println("Field number " + i + " : " + field.getName() + ",   Type: " + field.getType().getName() + ";");
+            i++;
+        }
+    }
+    static void printOnlyStaticFields(Class c) {
+        Field[] fields = c.getDeclaredFields();
+        int i = 0;
+        for (Field field: fields) {
+            if (java.lang.reflect.Modifier.isStatic(field.getModifiers())) {
+                System.out.println("Static field number " + i + " : " + field.getName() + ",   Type: " + field.getType().getName() + ";");
+                i++;
+            }
+        }
+    }
+    static void printMethods(Class c) {
+        Method[] methods = c.getMethods();
+        int i = 0;
+        for (Method method: methods) {
+            Parameter[] parameters = method.getParameters();
+            Type[] types = new Type[parameters.length];
+            int j = 0;
+            for (Parameter parameter: parameters) {
+                 types[j] = parameter.getType();
+                j++;
+            }
+            System.out.print("Method number " + i + " : " + method.getName());
+            System.out.print(",    Quantity of parameters: " + parameters.length);
+            System.out.print(",    Types of parameters: ");
+            for (Type type: types) {
+                System.out.print(type.getTypeName());
+            }
+            System.out.println();
+            i++;
         }
     }
 }
--- classWork_2/src/main/java/priorityUser.java
+++
@@ -1,9 +0,0 @@
-public class priorityUser extends User {
-    private long bankId;
-
-    public priorityUser(long id, String userName, long number, long bankId) {
-        super(id, userName, number);
-        this.bankId = bankId;
-    }
-
-}
