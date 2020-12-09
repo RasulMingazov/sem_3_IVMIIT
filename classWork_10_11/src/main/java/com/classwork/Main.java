bf7577d8373ae96cda6c08bf5434a92f96e18c50
RasulMingazov <59110467+RasulMingazov@users.noreply.github.com>
2020-12-09 05:58:58 +0300

    -

--- classWork_10_11/src/main/java/com/classwork/Main.java
+++ classWork_10_11/src/main/java/com/classwork/Main.java
@@ -6,19 +6,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.*;
-import java.util.*;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.*;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
